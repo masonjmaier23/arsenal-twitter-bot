95a85162eb656cb8e3bc19d0fde95b1865dc3eaf
Mason Maier <masonjmaier@gmail.com>
2022-06-15 15:13:55 -0700

    fixed spacing

--- src/twitterBotJob.js
+++ src/twitterBotJob.js
@@ -6,7 +6,6 @@ const europaLeaugeFinal = new Date("2023-05-31")
 const days = (europaLeaugeFinal.getTime() - currentDate.getTime()) / (1000 * 3600 * 24)
 const content = (Math.ceil(days) + " days until Europa Leauge glory!")
 
-
 const tweet = async () => {
 	try {
 		await rwClient.v2.tweet(content)
