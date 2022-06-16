10434d68a16654077f2755e95834400e8e8a4a30
Mason Maier <masonjmaier@gmail.com>
2022-06-16 08:28:13 -0700

    updated twitter profile and changed countdown to prem season

--- src/twitterBotJob.js
+++ src/twitterBotJob.js
@@ -2,9 +2,10 @@ const rwClient = require("./twitterClient.js")
 const cron = require("node-cron");
 
 const currentDate = new Date();
-const europaLeaugeFinal = new Date("2023-05-31")
-const days = (europaLeaugeFinal.getTime() - currentDate.getTime()) / (1000 * 3600 * 24)
-const content = (Math.ceil(days) + " days until Europa Leauge glory!")
+// const europaLeaugeFinal = new Date("2023-05-31")
+const firstPremGame = new Date("2023-08-05")
+const days = (firstPremGame.getTime() - currentDate.getTime()) / (1000 * 3600 * 24)
+const content = (Math.ceil(days) + " day(s) until our Premier Leauge opener against Crystal Palace! #COYG")
 
 const tweet = async () => {
 	try {
