ae3a5dc16b2fbd051402f193ce84fd441b8d19e2
Mason Maier <masonjmaier@gmail.com>
2022-06-23 09:10:47 -0700

    changed time to 10 am

--- src/twitterBotJob.js
+++ src/twitterBotJob.js
@@ -7,10 +7,10 @@ const config = {
 	method: "get",
 	url: "https://v3.football.api-sports.io/fixtures?season=2022&team=42&next=2",
 	headers: {
-	  "x-rapidapi-key": process.env.X_RAPIDAPI_KEY,
-	  "x-rapidapi-host": process.env.X_RAPIDAPI_HOST
+		"x-rapidapi-key": process.env.X_RAPIDAPI_KEY,
+		"x-rapidapi-host": process.env.X_RAPIDAPI_HOST
 	}
-  };
+};
 
 const currentDate = new Date();
 const firstPremGame = new Date("2023-08-05")
@@ -18,26 +18,26 @@ const days = (firstPremGame.getTime() - currentDate.getTime()) / (1000 * 3600 * 
 
 const tweetBot = async () => {
 	axios(config)
-  .then(function (fixtures) {
-	const fixtureDate = new Date(fixtures.data.response[0].fixture.date)
-	const days = (fixtureDate.getTime() - currentDate.getTime()) / (1000 * 3600 * 24)
-	const opponent = 
-		(fixtures.data.response[0].teams.home.name.includes("Arsenal") 
-			? fixtures.data.response[0].teams.away.name : 
-			fixtures.data.response[0].teams.home.name )
-	const stadium = fixtures.data.response[0].fixture.venue.name
-	const city = fixtures.data.response[0].fixture.venue.city
-	// const fixtureId = fixtures.data.response[0].fixture.id
+		.then(function (fixtures) {
+			const fixtureDate = new Date(fixtures.data.response[0].fixture.date)
+			const days = (fixtureDate.getTime() - currentDate.getTime()) / (1000 * 3600 * 24)
+			const opponent =
+				(fixtures.data.response[0].teams.home.name.includes("Arsenal")
+					? fixtures.data.response[0].teams.away.name :
+					fixtures.data.response[0].teams.home.name)
+			const stadium = fixtures.data.response[0].fixture.venue.name
+			const city = fixtures.data.response[0].fixture.venue.city
+			// const fixtureId = fixtures.data.response[0].fixture.id
 
-	const content = (Math.ceil(days) + ` day(s) til we play ${opponent} at ${stadium}, ${city}`)
-	rwClient.v2.tweet(content)
-  })
-  .catch(function (error) {
-	console.log(error);
-  });
+			const content = (Math.ceil(days) + ` day(s) til we play ${opponent} at ${stadium}, ${city}`)
+			rwClient.v2.tweet(content)
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
 }
 
-const job = cron.schedule("0 15 * * *", () => {
+const job = cron.schedule("0 17 * * *", () => {
 	tweetBot()
 	console.log("Successfully sent a tweet at: " + new Date())
 })
